f15e764ef3ec099e1c5d973405633610d3b1b969
Mu-nah <102527479+Mu-nah@users.noreply.github.com>
2025-10-11 00:46:34 +0100

    Update solbot.py

--- solbot.py
+++ solbot.py
@@ -146,4 +146,4 @@ def home():
 if __name__ == "__main__":
     thread = threading.Thread(target=bot_loop, daemon=True)
     thread.start()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000")))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
